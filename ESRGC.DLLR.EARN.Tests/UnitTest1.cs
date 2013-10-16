d0d3d962663424498e0b183bf65f92c120d84c74
tahoang <dhoang519@gmail.com>
2013-10-16 17:06:10 -0400

    Added geocoding class library project. Used to locate addresses

---
+++ ESRGC.DLLR.EARN.Tests/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ESRGC.GIS.Geocoding;
+using ESRGC.GIS.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ESRGC.DLLR.EARN.Tests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1() {
+      //arrage
+
+      //act
+      var req = Net.sendRequestString("http://mdimap.us/ArcGIS/rest/services/GeocodeServices/MD.State.MDCascadingLocatorWithZIPCodes/GeocodeServer/findAddressCandidates?Street=1455+E+Sandy+Acres+Dr&City=&ZIP=&SingleLine=&outFields=&outSR=4326&f=pjson");
+
+      //assert
+
+      Console.WriteLine(req);
+      Assert.IsNotNull(req);
+
+    }
+    [TestMethod]
+    public void TestParseJSon() {
+      //arrange
+      string url = "http://mdimap.us/ArcGIS/rest/services/GeocodeServices/MD.State.MDCascadingLocatorWithZIPCodes/GeocodeServer/findAddressCandidates?Street=1455+E+Sandy+Acres+Dr&City=&ZIP=&SingleLine=&outFields=&outSR=4326&f=pjson";
+      //act
+      var result = Net.sendRequestString(url);
+      var resultObj = Net.deserializeJson(result);
+      //assert
+      Console.WriteLine(resultObj.ToString());
+      //Assert.AreEqual(4326, resultObj.spatialReference.wkid);
+
+    }
+
+    [TestMethod]
+    public void TestAddressGeocode() {
+      //arrange
+      var address = "1455 E Sandy Acres Dr";
+      var city = "Salisbury";
+      var zip = "21804";
+      var geocoder = new MDLocatorWithZip();//default lat/lon
+      //act
+      var result = geocoder.geocode(address, city, zip);
+      var resultObj = Net.deserializeJson(result);
+      var candidates = resultObj.candidates as IEnumerable<dynamic>;
+
+      Console.WriteLine(geocoder.Url);
+
+      foreach (var candidate in candidates) {
+        if (candidate.score == 100)
+          Console.WriteLine(candidate);
+      }
+
+
+    }
+  }
+}
---
+++ ESRGC.GIS/Geocoding/MDLocatorWithZip.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ESRGC.GIS.Utilities;
+
+namespace ESRGC.GIS.Geocoding
+{
+  public class MDLocatorWithZip : Geocoder
+  {
+    string _geocodeUrl, _reverseGeocodeUrl;
+
+    public string ReverseGeocodeUrl {
+      get { return _reverseGeocodeUrl; }
+    }
+
+    public string GeocodeUrl {
+      get { return _geocodeUrl; }
+    }
+    /// <summary>
+    /// MD cascading locator with zip code.
+    /// Default constructor that uses Lat/Lon projection (4326)
+    /// </summary>
+    public MDLocatorWithZip() : this(4326) { }
+
+    /// <summary>
+    /// MD cascading locator with zip code
+    /// </summary>
+    /// <param name="wkid">Spatial reference</param>
+    public MDLocatorWithZip(int wkid) {
+      _url = "http://mdimap.us/ArcGIS/rest/services/GeocodeServices/MD.State.MDCascadingLocatorWithZIPCodes/GeocodeServer";
+      _geocodeUrl = _url + "/findAddressCandidates?f=pjson";
+      _reverseGeocodeUrl = _url + "/reverseGeocode?f=pjson";
+      //add spatial reference
+      _geocodeUrl += "&outSR=" + wkid;
+      _reverseGeocodeUrl += "&outSR=" + wkid;
+      _wkid = wkid;
+    }
+    /// <summary>
+    /// geocode address and zip code
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="zip"></param>
+    /// <returns>JSON result</returns>
+    public override string geocode(string address, string zip) {
+      address = address.Replace("  ", " ").Trim();//remove double white space
+      zip = zip.Trim();
+      //construct url
+      var url = _geocodeUrl + string.Format("&street={0}&zip={1}", address, zip);
+
+      return Net.sendRequestString(url);
+    }
+    /// <summary>
+    /// geocode address, zipcode, and city
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="city"></param>
+    /// <param name="zip"></param>
+    /// <returns>Json result</returns>
+    public override string geocode(string address, string city, string zip) {
+      address = address.Replace("  ", " ").Trim();//remove double white space
+      zip = zip.Trim();
+      city = city.Trim();
+      //construct url
+      var url = _geocodeUrl + string.Format("&street={0}&zip={1}&city={2}", address, zip, city);
+
+      return Net.sendRequestString(url);
+    }
+    /// <summary>
+    /// reverse geocode X,Y location
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="distance">distance from center</param>
+    /// <returns>JSon result</returns>
+    public override string reverseGeocode(double x, double y, double distance) {
+      try {
+        var url = _reverseGeocodeUrl + string.Format("location={0}%2C{1}&distance={2}", x, y, distance);
+        return Net.sendRequestString(url);
+      }
+      catch {
+        return null;
+      }
+    }
+  }
+}
