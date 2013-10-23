e30db98dea66ae2d9f4c032c3b2feca2198ca267
tahoang <dhoang519@gmail.com>
2013-10-23 16:18:37 -0400

    Edited css classes to use fontawesome 4.0.0

--- ESRGC.DLLR.EARN/Controllers/PartnershipController.cs
+++ ESRGC.DLLR.EARN/Controllers/PartnershipController.cs
@@ -6,15 +6,46 @@ using System.Web.Mvc;
 
 namespace ESRGC.DLLR.EARN.Controllers
 {
-    public class PartnershipController : BaseController
-    {
-        //
-        // GET: /Partnership/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+  public class PartnershipController : BaseController
+  {
+    //
+    // GET: /Partnership/
+    //for search
+    public ActionResult Index() {
+      return View();
     }
+    /// <summary>
+    /// View partnership detail
+    /// </summary>
+    /// <param name="partnershipID"></param>
+    /// <returns></returns>
+    public ActionResult Detail(int partnershipID) {
+      return View();
+    }
+    /// <summary>
+    /// create a new partnership
+    /// </summary>
+    /// <returns></returns>
+    public ActionResult Create() {
+      return View();
+    }
+    /// <summary>
+    /// Manage partnership
+    /// </summary>
+    /// <param name="partnershipID"></param>
+    /// <returns></returns>
+    public ActionResult Edit(int partnershipID) {
+      return View();
+    }
+    /// <summary>
+    /// Delete a partnership
+    /// </summary>
+    /// <param name="partnershipID"></param>
+    /// <returns></returns>
+    public ActionResult Delete(int partnershipID) {
+      return View();
+    }
+
+
+  }
 }
