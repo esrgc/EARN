5228d7af27a6e4260c89d3edc81ac28f1a4390d0
tahoang <dhoang519@gmail.com>
2013-12-02 17:39:50 -0500

    Added dashboard (simple) admin access only

---
+++ ESRGC.DLLR.EARN/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ESRGC.DLLR.EARN.Domain.DAL.Abstract;
+using ESRGC.DLLR.EARN.Filters;
+using ESRGC.DLLR.EARN.Models;
+
+namespace ESRGC.DLLR.EARN.Controllers
+{
+  [Authorize]
+  [RoleAuthorize(Roles = "admin")]
+  [VerifyProfile]
+  public class DashboardController : BaseController
+  {
+    public DashboardController(IWorkUnit workUnit) : base(workUnit) { }
+    [AllowNonAdmin]
+    public ActionResult NavLinks() {
+      if (CurrentAccount.Role.ToLower() == "admin")
+        return PartialView();
+      else
+        return new EmptyResult();
+    }
+    public ActionResult Index() {
+      var stats = new DashboardStatistic {
+        AccountTotal = _workUnit.AccountRepository.Entities.Count(),
+        ProfileTotal = _workUnit.ProfileRepository.Entities.Count(),
+        ContactTotal = _workUnit.ContactRepository.Entities.Count(),
+        Profiles = _workUnit.ProfileRepository.Entities.OrderBy(x => x.Organization.Name).ToList(),
+        Partnerships = _workUnit.PartnershipRepository.Entities.OrderBy(x => x.Name).ToList(),
+        OrganizationTotal = _workUnit.OrganizationRepository.Entities.Count()
+      };
+      return View(stats);
+    }
+
+  }
+}
---
+++ ESRGC.DLLR.EARN/Filters/RoleAuthorizeAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ESRGC.DLLR.EARN.Controllers;
+
+namespace ESRGC.DLLR.EARN.Filters
+{
+  public class RoleAuthorizeAttribute : AuthorizeAttribute
+  {
+    public override void OnAuthorization(AuthorizationContext filterContext) {
+      if (filterContext.ActionDescriptor.GetCustomAttributes(typeof(AllowNonAdminAttribute), false).Any()) {
+        // The controller action is decorated with the [AllowNonProfileAttribute]
+        // custom attribute => don't do anything.
+        return;
+      }
+      var requestEmail = filterContext.HttpContext.User.Identity.Name;
+      var workUnit = (filterContext.Controller as BaseController).WorkUnit;
+      try {
+        var account = workUnit
+          .AccountRepository
+          .Entities
+          .First(x => x.EmailAddress.ToLower() == requestEmail.ToLower());
+        
+        //check if the current profile is in the partnership partners list
+        if (!Roles.Contains(account.Role.ToLower())) {
+          filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary() { 
+            {"controller", "Home"},
+            {"action", "UnauthorizedAccess"}
+          });
+        }
+      }
+      catch (Exception) {
+        filterContext.Controller.TempData["Message"] = "Error verifying role for the current account";
+        filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary() { 
+          {"controller", "Home"},
+          {"action", "Index"}
+        });
+      }
+    }
+    
+  }
+}
